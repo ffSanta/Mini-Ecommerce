7668dfd6e8d86db2dbcb30886c7e095a3dfe69a2
inzac <beginalnone@gmail.com>
2026-02-12 22:04:08 +0700

    add login register page&controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',[LoginController::class,'create']);
+Route::PUT('/login',[LoginController::class,'store']);
+
+Route::get('/register',[RegisterController::class,'create']);
+Route::PUT('/register',[RegisterController::class,'store']);
